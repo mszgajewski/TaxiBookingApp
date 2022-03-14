3416102f31edab52707fb5a92bb1651313f6f46d
mszgajewski <mszgajewski@gmail.com>
2022-03-14 21:32:25 +0100

    Initial Commit

--- app/src/main/java/com/mszgajewski/taxibookingapp/PhoneLoginActivity.java
+++ app/src/main/java/com/mszgajewski/taxibookingapp/PhoneLoginActivity.java
@@ -3,12 +3,17 @@ package com.mszgajewski.taxibookingapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.hbb20.CountryCodePicker;
 
 public class PhoneLoginActivity extends AppCompatActivity {
 
     CountryCodePicker ccp;
+    private EditText phoneEditText;
     private String selected_country_code = "+48";
 
     @Override
@@ -16,13 +21,34 @@ public class PhoneLoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_phone_login);
 
-        ccp = findViewById(R.id.ccp);
+        ccp = (CountryCodePicker) findViewById(R.id.ccp);
+        phoneEditText = (EditText) findViewById(R.id.editTextTextPersonName2);
 
         ccp.setOnCountryChangeListener(new CountryCodePicker.OnCountryChangeListener() {
             @Override
             public void onCountrySelected() {
                 selected_country_code = ccp.getSelectedCountryCodeWithPlus();
                 ccp.setCountryPreference("PL");
+            }
+        });
+
+        phoneEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+                if(charSequence.toString().length() == 9) {
+                    Toast.makeText(PhoneLoginActivity.this,"halo", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
             }
         });
     }
